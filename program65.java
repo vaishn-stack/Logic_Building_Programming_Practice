6bfd93807e4dad2ee967ed005a71ca5bacecf97d
Vaishnavi Deepak Morankar <vaishnavimorankar296@gmail.com>
2026-05-29 17:40:15 +0530

    Add files via upload

---
+++ program65.java
@@ -0,0 +1,62 @@
+// Prime number
+
+import java.util.Scanner;
+
+class NumberX
+{
+
+     public boolean CheckPrime(int iNo)
+     {
+          int iCnt = 0;
+         
+          for (iCnt = 2; iCnt <= (iNo / 2); iCnt++) 
+          {
+              if((iNo % iCnt) == 0)
+              {
+                    break;
+              }
+          }
+          
+          if(iCnt >= (iNo / 2) + 1)
+          {
+               return true;
+          }
+          else
+          {
+               return false;
+          }
+     }
+}
+
+public class program65
+{
+     public static void main(String A[])     
+     { 
+          Scanner sobj = new Scanner(System.in);
+
+          int iValue = 0; 
+          boolean bRet = false;
+
+          System.out.println("Enter number : ");
+          iValue = sobj.nextInt();
+
+          System.out.println("---------------------");
+
+          NumberX nobj = new NumberX();  
+
+          bRet = nobj.CheckPrime(iValue);
+
+          if(bRet == true)
+          {
+               System.out.println("It is Prime.");
+          }
+          else
+          {
+               System.out.println("It is not Prime.");
+          }
+          
+     }
+}
+
+// Time Complexity : O(N / 2)
+// Where N >= 0
